9446ae1c74dea90c322a7c658ed413e9b0842ed7
Abhi Basu Thakur <abhibt@yahoo.com>
2019-12-28 16:36:11 +0530

    Uploaded after Ads Display started
    Uploaded after Ads Display started

--- app/src/main/java/com/example/abcd/MainActivity.java
+++ app/src/main/java/com/example/abcd/MainActivity.java
@@ -7,9 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    private AdView adView;
     static int nCharVal = 65; //Starting with A
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         vText.setTextColor(Color.GREEN);
         String szString = (String)Character.toString((char)nCharVal);
         vText.setText(szString);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+        // values/strings.xml.
+        adView = findViewById(R.id.adView1);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
     }
   // @Override
   public void perform_action(View V)
@@ -35,7 +53,10 @@ public class MainActivity extends AppCompatActivity {
            nCharVal = 64;
        }
 
-       //assign the textview forecolor https://developers.google.com/admob/android/quick-start#initialize_mobile_ads
+       //assign the textview forecolor https://developers.google.com/admob/android/quick-start#initialize_mobile_ads  ca-app-pub-9546661915028941~2419598217
+       // ca-app-pub-9546661915028941/9910790018
+       //https://apps.admob.com/v2/apps/2419598217/adunits/list?utm_source=internal&utm_medium=et&utm_campaign=helpcentrecontextualopt&utm_term=http%3A%2F%2Fgoo.gl%2F6Xkfcf&subid=ww-ww-et-amhelpv4
+       //https://github.com/googleads/googleads-mobile-android-examples/blob/master/java/admob/BannerExample/app/src/main/AndroidManifest.xml
 
        tv.setTextColor(Color.GREEN);
    }
